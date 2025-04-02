3ac95ea0f8ae4bb4345436e3e0103a6c249e9b9d
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-02 14:38:49 -0300

    testing deploy

---
+++ scripts/exec/setValueTestContract.ts
@@ -0,0 +1,15 @@
+  import hre, { ethers } from "hardhat";
+
+  
+  async function main() {
+   
+    const helloWorld = await ethers.getContractAt("TestContract", "0xFB756d997431460deE22D3B7A7D23dDC16E89B69");
+    await helloWorld.setValue("99");    
+    console.log("Value set!");
+
+    console.log("Getting value");
+    console.log(await helloWorld.value());
+  }
+
+  main();
+  
