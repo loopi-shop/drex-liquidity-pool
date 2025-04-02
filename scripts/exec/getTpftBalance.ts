a77c5d6260562317395a643d9f68ca3bbeccc910
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-02 15:45:28 -0300

    Adding test scripts

---
+++ scripts/exec/getTpftBalance.ts
@@ -0,0 +1,26 @@
+import hre, { ethers } from "hardhat";
+import * as config from "../config/config";
+import abiTpft from "../../abis/ITPFt.json";
+
+async function main() {
+
+  /**
+ * Obtém contrato TPFt
+ */
+  const TPFt = await ethers.getContractAt(
+    abiTpft,
+    config.TPFT_ADDRESS
+  );
+
+
+  const balanceOfTPFt = await TPFt.balanceOf(
+    config.MAIN_WALLET_ADDRESS,
+    1
+  );
+
+  console.log("TPFt balance", balanceOfTPFt);
+
+}
+
+
+main();
