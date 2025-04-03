06a10bcaa8a833fc92d6baa4085636763aadb8b7
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-03 15:35:17 -0300

    testing transfer

--- scripts/init/setDrexPermissions.ts
+++ scripts/init/setDrexPermissions.ts
@@ -1,27 +1,15 @@
 import hre, { ethers } from "hardhat";
 import * as config from "../config/config";
-import abiAccountRules from '../../abis/AccountRules.json';
+import abiSTR from "../../abis/STR.json";
+import abiRealDigitalEnableAccount from "../../abis/RealDigitalEnableAccount.json";
 
 async function main() {
 
-      //0x51FD57d1f7c9539986333C50978D2E9926300e27 - prod nova versão 10/03/25
-      const rulesInstance = await ethers.getContractAt(abiAccountRules, "0x51FD57d1f7c9539986333C50978D2E9926300e27");
-
-
-      const listedContracts:Array<String> = [config.WRAPPER_ADDRESS, config.POOL_ADDRESS];
-      
-      for (let i = 0; i < listedContracts.length; i++) {
-  
-          //realiza a permissão de um contrato para ser executado na rede
-          const contract = listedContracts[i];
-          await (await rulesInstance.addTarget(contract.toString())).wait();
-          
-          //Caso queira remover o contrato para não ser mais executado, utilize o removeTarget
-          //await (await rulesInstance.removeTarget(contract.toString())).wait();
-          
-  
-          console.log("Contract added:", contract);
-      }   
+    const STR = await ethers.getContractAt(abiSTR, config.STR_ADDRESS);
+    const enableAccount = await ethers.getContractAt(abiRealDigitalEnableAccount, config.REAL_DIGITAL_ENABLE_ACCOUNT_ADDRESS);
+    
+    // Após ter um endereço habilitado pelo BACEN, a instituição pode habilitar novos endereços
+    await enableAccount.enableAccount("0x7E4bd9599324EdE4d15454C3fAC2c4a07A59781B");
   
 }
 
