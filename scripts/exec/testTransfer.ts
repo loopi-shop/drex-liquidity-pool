ee8ca25182bd2089722d39f4599fa28f5e86a456
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-03 15:38:52 -0300

    testing transfer

--- scripts/exec/testTransfer.ts
+++ scripts/exec/testTransfer.ts
@@ -1,7 +1,6 @@
 import hre, { ethers } from "hardhat";
 import * as config from "../config/config";
 import abiRealDigital from '../../abis/RealDigital.json';
-import abiTpftIERC1155 from "../../abis/IERC1155.json";
 // import abiRealDigitalDefaultAccount from '../abi/RealDigitalDefaultAccount.json';
 
 
@@ -15,7 +14,7 @@ async function main() {
 
   console.log("Transfer drex!");
 
-  const wTpft = await ethers.getContractAt(abiTpftIERC1155, config.WRAPPER_ADDRESS);
+  const wTpft = await ethers.getContractAt("ERC1155ToERC20Wrapper", config.WRAPPER_ADDRESS);
 
   await wTpft.transfer("0x7E4bd9599324EdE4d15454C3fAC2c4a07A59781B", 1);
 
