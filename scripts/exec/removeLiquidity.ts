ed2b9cac764f28ffd201df60c3af60a5ed72c67b
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-07 11:22:32 -0300

    testing swap

--- scripts/exec/removeLiquidity.ts
+++ scripts/exec/removeLiquidity.ts
@@ -5,18 +5,19 @@ import { sleep } from "../util/util";
 
 async function main() {
   const [owner] = await ethers.getSigners();
+  const user = new ethers.Wallet(config.USER_WALLET_PK, ethers.provider);
 
   const simplePool = await ethers.getContractAt("SimplePoolProductConstant", config.POOL_ADDRESS);
 
-  let shares = await simplePool.balanceOf(owner.address);
+  let shares = await simplePool.balanceOf(user.address);
   console.log("Total shares antes:", shares);
 
-  let transaction = await simplePool.removeLiquidity(shares)
+  let transaction = await simplePool.connect(user).removeLiquidity(shares)
   console.log("RemoveLiquidity realizado! Transação", transaction.hash);
 
   await sleep(10000);
 
-  shares = await simplePool.balanceOf(owner.address);
+  shares = await simplePool.balanceOf(user.address);
 
   console.log("Total shares depois:", shares);
 }
--- scripts/exec/swapTpftInDrexOut.ts
+++ scripts/exec/swapTpftInDrexOut.ts
@@ -9,13 +9,14 @@ import { sleep } from "../util/util";
 
 async function main() {
   const [owner] = await ethers.getSigners();
+  const user = new ethers.Wallet(config.USER_WALLET_PK, ethers.provider);
 
   const simplePool = await ethers.getContractAt("SimplePoolProductConstant", config.POOL_ADDRESS);
   const drex = await ethers.getContractAt(abiRealDigital, config.DREX_ADDRESS);
   const TPFt = await ethers.getContractAt(abiTpft, config.TPFT_ADDRESS);
 
-  let balanceDrex = await drex.balanceOf(config.MAIN_WALLET_ADDRESS);
-  let balanceOfTPFt = await TPFt.balanceOf(config.MAIN_WALLET_ADDRESS, config.TPFT_ID);
+  let balanceDrex = await drex.balanceOf(user.address);
+  let balanceOfTPFt = await TPFt.balanceOf(user.address, config.TPFT_ID);
 
   console.log("Saldo Drex antes:", balanceDrex);
   console.log("Saldo TPFt antes:", balanceOfTPFt);
@@ -25,13 +26,13 @@ async function main() {
 
   console.log(`Estimativa de retorno para ${amountIn} tpft: `, amountOut);
 
-  let transaction = await simplePool.swap(config.WRAPPER_ADDRESS, amountIn);
+  let transaction = await simplePool.connect(user).swap(config.WRAPPER_ADDRESS, amountIn);
   console.log("Swap realizado! Transação", transaction.hash);
 
   await sleep(10000);
 
-  balanceDrex = await drex.balanceOf(config.MAIN_WALLET_ADDRESS);
-  balanceOfTPFt = await TPFt.balanceOf(config.MAIN_WALLET_ADDRESS, config.TPFT_ID);
+  balanceDrex = await drex.balanceOf(user.address);
+  balanceOfTPFt = await TPFt.balanceOf(user.address, config.TPFT_ID);
 
   console.log("Saldo Drex depois:", balanceDrex);
   console.log("Saldo TPFt depois:", balanceOfTPFt);
