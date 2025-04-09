8efccc362eaaa4fbf3b517240400a25e4986104e
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-09 15:36:37 -0300

    Improving scripts and docs

--- scripts/info/getDrexAndTPFTBalance.ts
+++ scripts/info/getDrexAndTPFTBalance.ts
@@ -11,8 +11,8 @@ async function main() {
   const user = new ethers.Wallet(config.USER_WALLET_PK, ethers.provider);
   const drex = await ethers.getContractAt(abiRealDigital, config.DREX_ADDRESS);
 
-  console.log("Saldo Drex", await drex.balanceOf(owner.address));
-  console.log("Decimals Drex", await drex.decimals());
+  console.log("Saldo Drex", (await drex.balanceOf(owner.address)).toString());
+  // console.log("Decimals Drex", await drex.decimals());
 
 
 
@@ -24,46 +24,46 @@ async function main() {
     config.TPFT_ADDRESS
   );
 
-  for (let index = 0; index < 5; index++) {
+  // for (let index = 0; index < 5; index++) {
     const balanceOfTPFt = await TPFt.balanceOf(
       owner.address,
-      index
+      config.TPFT_ID
     )
   
     //Saldo do TPFt.
-    console.log(`Saldo TPFt id ${index}`,balanceOfTPFt.toString());
+    console.log(`Saldo TPFt id ${config.TPFT_ID}`, balanceOfTPFt.toString());
     
     
-  }
+  // }
 
-  console.log("Decimals Tpft", await TPFt.decimals());
+  // console.log("Decimals Tpft", await TPFt.decimals());
 
-  console.log("TpftId", await TPFt.getTPFtId(
-    {
-      acronym: 'LTN',
-      code: '100000',
-      // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
-      maturityDate: 1733011200,
-    }
-  ));
+  // console.log("TpftId", await TPFt.getTPFtId(
+  //   {
+  //     acronym: 'LTN',
+  //     code: '100000',
+  //     // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
+  //     maturityDate: 1733011200,
+  //   }
+  // ));
   
-  console.log("TpftId", await TPFt.getTPFtId(
-    {
-      acronym: 'LTN',
-      code: '100000',
-      // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
-      maturityDate: 1731628800,
-    }
-  ));
+  // console.log("TpftId", await TPFt.getTPFtId(
+  //   {
+  //     acronym: 'LTN',
+  //     code: '100000',
+  //     // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
+  //     maturityDate: 1731628800,
+  //   }
+  // ));
 
-  console.log("TpftId", await TPFt.getTPFtId(
-    {
-      acronym: 'LTN',
-      code: '100000',
-      // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
-      maturityDate: 1703203200,
-    }
-  ));
+  // console.log("TpftId", await TPFt.getTPFtId(
+  //   {
+  //     acronym: 'LTN',
+  //     code: '100000',
+  //     // A função Math.floor(date.getTime() / 1000) transformar data milissegundos em segundos(timestamp Unix)
+  //     maturityDate: 1703203200,
+  //   }
+  // ));
 
 }
 
