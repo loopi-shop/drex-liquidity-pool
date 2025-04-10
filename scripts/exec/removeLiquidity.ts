0d67accd30a57ebde550888cac0e1f7aaed68fcd
Thiago Kafouri <kafouri@Thiagos-MacBook-Air.local>
2025-04-10 04:25:10 -0300

    Fix multiple add liquidity

--- scripts/exec/removeLiquidity.ts
+++ scripts/exec/removeLiquidity.ts
@@ -5,20 +5,20 @@ import { printBalances } from "./utils";
 
 
 async function main() {
-  const [owner] = await ethers.getSigners();
+  // const [owner] = await ethers.getSigners();
   const user = new ethers.Wallet(config.USER_WALLET_PK, ethers.provider);
 
   const simplePool = await ethers.getContractAt("SimplePoolProductConstant", config.POOL_ADDRESS);
 
-  await printBalances(owner.address);
+  await printBalances(user.address);
 
-  let shares = await simplePool.balanceOf(owner.address);
-  let transaction = await simplePool.connect(owner).removeLiquidity(shares)
+  let shares = await simplePool.balanceOf(user.address);
+  let transaction = await simplePool.connect(user).removeLiquidity(shares)
   console.log("Liquidez removida! Transação", transaction.hash);
 
   await sleep(10000);
 
-  await printBalances(owner.address);
+  await printBalances(user.address);
   
 }
 
